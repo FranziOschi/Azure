1c5e70837ffe5894eb7eca300410dc8b98920e9f
Franziska Oschmann <franziskaoschmann@staff-net-als-2042.ethz.ch>
2019-03-15 14:51:05 +0100

    first commit

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
-
 apt-get update -y && apt-get upgrade -y
-apt-get install -y ipython
+apt-get install -y nginx
+echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
