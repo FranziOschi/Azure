0d55cb4f046303d512b2485c61e4287f5c67f6f5
Franziska Oschmann <franziskaoschmann@staff-net-als-2042.ethz.ch>
2019-03-15 14:53:02 +0100

    first commit

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 apt-get update -y && apt-get upgrade -y
 apt-get install -y nginx
+apt-get install -y python3-pip
+pip3 install pip matplotlib jupyter pandas --user
 echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
