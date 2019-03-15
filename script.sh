fef42eea5270422a5ffe7093e47bfcb5f0b98052
Franziska Oschmann <franziskaoschmann@staff-net-als-2042.ethz.ch>
2019-03-15 11:33:26 +0100

    first_commit

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #echo "Updating packages ..."
 
-#export LC_ALL="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
 sudo apt -y update
-sudo apt -y install python-pip
 sudo apt -y install python3-pip
 pip3 install pip matplotlib jupyter pandas --user
